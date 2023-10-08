b555fb32514b877d91786dbe38108231ea5cddef
IvanPolitecnics <100274553+IvanPolitecnics@users.noreply.github.com>
2023-10-08 17:05:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,71 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Obtener referencias a elementos HTML
+    const propinaForm = document.getElementById("propinaForm");
+    const calcularBtn = document.getElementById("calcular");
+    const resultadoDiv = document.getElementById("resultado");
+    const propinaPersonaP = document.getElementById("propinaPersona");
+
+    // Función para realizar el cálculo de la propina
+    function calcula() {
+        // Obtener valores ingresados por el usuario desde el formulario
+        const comensales = parseInt(propinaForm.comensales.value);
+        const totalCuenta = parseFloat(propinaForm.totalCuenta.value);
+        const servicio = propinaForm.servicio.value;
+
+        // Validar que los valores ingresados sean números válidos
+        if (isNaN(comensales) || isNaN(totalCuenta)) {
+            alert("Por favor, ingresa números válidos.");
+            return;
+        }
+
+        // Determinar el porcentaje de propina basado en la calificación del servicio
+        let porcentajePropina = 0;
+        switch (servicio) {
+            case "genial":
+                porcentajePropina = 10;
+                break;
+            case "aceptable":
+                porcentajePropina = 5;
+                break;
+            case "horrible":
+                porcentajePropina = 0;
+                break;
+            default:
+                alert("Selecciona una calificación de servicio válida.");
+                return;
+        }
+
+        // Calcular la cantidad total de propina
+        let propinaTotal = (totalCuenta * porcentajePropina) / 100;
+
+        // Verificar si se debe agregar una propina fija de 0.5€ por atenderles
+        if (propinaForm.propinaAtencion.checked) {
+            propinaTotal += 0.5;
+        }
+
+        // Calcular la propina por persona
+        let propinaPorPersona = propinaTotal / comensales;
+
+        // Verificar que la cantidad de propina por persona no sea menor a 0.50 €
+        if (propinaPorPersona < 0.5) {
+            alert("La propina por persona no puede ser inferior a 0.50 €.");
+            return;
+        }
+
+        // Mostrar el resultado en la página
+        resultadoDiv.style.display = "block";
+        propinaPersonaP.textContent = propinaPorPersona.toFixed(2) + " €";
+
+        // Limpiar el formulario
+        propinaForm.reset();
+    }
+
+    // Asociar la función calcula() al botón "Calcular"
+    calcularBtn.addEventListener("click", calcula);
+});
+
+
+/*document.addEventListener("DOMContentLoaded", function () {
     const propinaForm = document.getElementById("propinaForm");
     const calcularBtn = document.getElementById("calcular");
     const resultadoDiv = document.getElementById("resultado");
@@ -53,3 +120,69 @@ document.addEventListener("DOMContentLoaded", function () {
         propinaForm.reset();
     });
 });
+*/
+/*
+
++++CON 0.5€ DE PROPINA SOLO POR ATENDER+++
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtener referencias a elementos HTML
+    const propinaForm = document.getElementById("propinaForm");
+    const calcularBtn = document.getElementById("calcular");
+    const resultadoDiv = document.getElementById("resultado");
+    const propinaPersonaP = document.getElementById("propinaPersona");
+
+    // Función para realizar el cálculo de la propina
+    function calcula() {
+        // Obtener valores ingresados por el usuario desde el formulario
+        const comensales = parseInt(propinaForm.comensales.value);
+        const totalCuenta = parseFloat(propinaForm.totalCuenta.value);
+        const servicio = propinaForm.servicio.value;
+
+        // Validar que los valores ingresados sean números válidos
+        if (isNaN(comensales) || isNaN(totalCuenta)) {
+            alert("Por favor, ingresa números válidos.");
+            return;
+        }
+
+        // Determinar el porcentaje de propina basado en la calificación del servicio
+        let porcentajePropina = 0;
+        switch (servicio) {
+            case "genial":
+                porcentajePropina = 10;
+                break;
+            case "aceptable":
+                porcentajePropina = 5;
+                break;
+            case "horrible":
+                porcentajePropina = 0;
+                break;
+            default:
+                alert("Selecciona una calificación de servicio válida.");
+                return;
+        }
+        
+        // Calcular la cantidad total de propina
+        let propinaTotal = (totalCuenta * porcentajePropina) / 100;
+        let propinaPorPersona = propinaTotal / comensales;
+
+        // Verificar que la cantidad de propina por persona no sea menor a 0.50 €
+        if (propinaPorPersona < 0.5) {
+            alert("La propina por persona no puede ser inferior a 0.50 €.");
+            return;
+        }
+
+        // Mostrar el resultado en la página
+        resultadoDiv.style.display = "block";
+        propinaPersonaP.textContent = propinaPorPersona.toFixed(2) + " €";
+
+        // Limpiar el formulario
+        propinaForm.reset();
+    }
+
+    // Asociar la función calcula() al botón "Calcular"
+    calcularBtn.addEventListener("click", calcula);
+});
+*/
+
+
